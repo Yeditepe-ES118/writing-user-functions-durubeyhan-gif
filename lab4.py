cabe0cdb7fa8b1d8328b942d45b48d2428507bd1
durubeyhan-gif <duru.beyhan@std.yeditepe.edu.tr>
2025-10-22 11:41:51 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -7,7 +7,7 @@ def throw_rock (m,v0,theta):
     R= v0**2 * np.sin(2* theta) #in m
     hm = v0 **2* np.sin(theta)**2/ (2*g) #in m
     vh= v0*np.cos(theta) #in m/s
-    Kh= 0.5 * m* vh**hm**2 #in J
+    Kh= 0.5 * m* vh* hm**2 #in J
     
     print("For a rock with %5.3f kg mass thrown with %5.3f m/s at an angle of %6.2f degrees:\n"\
           "Time of flight is %10.1e s\nThe range in x-direction is %10.1e m\n"\
