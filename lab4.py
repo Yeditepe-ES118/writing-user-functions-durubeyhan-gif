6e540502c45aff99ff6b8c2c745bd94110bcea19
durubeyhan-gif <duru.beyhan@std.yeditepe.edu.tr>
2025-10-22 11:31:32 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -15,5 +15,5 @@ def throw_rock (m,v0,theta):
           "Kinetic energy at the maximum height is %8.2e J\n" % (m,v0,theta*180/np.pi,tf,R,hm,vh,Kh))
     return tf,R,hm,vh,Kh
 
-myresult= throw_rock(1.5,0.3,35.20)
+
     
