3ac7a438e64d5b970e5ab3ddcbf9a019dcee42a9
durubeyhan-gif <duru.beyhan@std.yeditepe.edu.tr>
2025-10-23 12:59:16 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -1,15 +1,28 @@
 import numpy as np
 
-def throw_rock(m,v0,theta):
-    g=9.81 
-    theta=theta*np.pi/180 # now it is in radians
-    tf= 2*v0*np.sin(theta)/g
-    R=v0**2*np.sin(2*theta)/g
-    hm=v0*2*np.sin(theta)*2/(2*g)
-    vh=v0*np.cos(theta)
-    kh=(m*vh*hm**2)/2 # in joule (J)
-    print("For a rock with %5.3f kg mass thrown with %5.3f m/s at an angle of %6.2f degrees:\n"\
-          "Time of flight is %10.1e s\nThe range in x-direction is %10.1e m\n"\
-          "Maximum height is %10.1e m\nThe speed at maximum height is %10.1e m/s\n"\
-          "Kinetic energy at the maximum height is %8.2e J" % (m,v0,theta*180/np.pi,tf,R,hm,vh,kh))
+def throw_rock(m, v0, theta_deg):
+    g = 9.81
+    # Açıyı dereceyi radyana çevirmek için numpy kullanıldı
+    theta_rad = np.radians(theta_deg)
+
+    # DÜZELTİLMİŞ FİZİKSEL HESAPLAMALAR
+    tf = (2 * v0 * np.sin(theta_rad)) / g
+    R = (v0**2 * np.sin(2 * theta_rad)) / g
+    # Maksimum Yükseklik (hm) Formülü düzeltildi
+    hm = (v0**2 * (np.sin(theta_rad)**2)) / (2 * g)
+    vh = v0 * np.cos(theta_rad)
+    # Kinetik Enerji (kh) Formülü düzeltildi (K = 0.5 * m * vh^2)
+    kh = 0.5 * m * vh**2
+
+    # TESTİ GEÇMEK İÇİN ÇIKTI FORMATI AYARLAMASI (Boşluklara DİKKAT!)
+    # Not: np.pi yerine theta_deg kullanıldığı için np.degrees(theta_rad) kaldırıldı.
+    print(f"For a rock with {m:.3f} kg mass thrown with {v0:.3f} m/s at an angle of {theta_deg: 5.2f} degrees:")
+    
+    # "is" kelimesinden sonra 4 boşluk bırakılarak çıktı formatı eşleştirildi.
+    print(f"Time of flight is    {tf:.2e} s")
+    print(f"The range in x-direction is    {R:.2e} m")
+    print(f"Maximum height is    {hm:.2e} m")
+    print(f"The speed at maximum height is    {vh:.2e} m/s")
+    print(f"Kinetic energy at the maximum height is {kh:.2e} J")
+    
     return tf, R, hm, vh, kh
