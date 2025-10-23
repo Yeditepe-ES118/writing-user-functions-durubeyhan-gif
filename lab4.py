39e549d8d23693844761cd0b5ac0a719fe5eb923
durubeyhan-gif <duru.beyhan@std.yeditepe.edu.tr>
2025-10-23 12:27:15 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -8,8 +8,6 @@ def throw_rock(m,v0,theta):
     hm=v0*2*np.sin(theta)*2/(2*g)
     vh=v0*np.cos(theta)
     kh=(m*vh*hm**2)/2 # in joule (J)
-    
-    #eğer return burada olsaydı returnden sonra hiçbir şey değerlendirilmediği için tüm işlemşeri returnden önce yaparız
     print("For a rock with %5.3f kg mass thrown with %5.3f m/s at an angle of %6.2f degrees:\n"\
           "Time of flight is %10.1e s\nThe range in x-direction is %10.1e m\n"\
           "Maximum height is %10.1e m\nThe speed at maximum height is %10.1e m/s\n"\
