8e7c4fe9e0617f0e0f267506d03a6093e31e28a8
durubeyhan-gif <duru.beyhan@std.yeditepe.edu.tr>
2025-11-08 15:48:23 +0300

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -7,6 +7,6 @@ def throw_rock(m,v0,theta):
     vh= v0* np.cos(theta*np.pi/180)
     Kh= 0.5* m* vh* hm**2 #in joule
     
-    print("For a rock with %5.3f kg mass thrown with %5.3f m/s at an angle of %5.2f degrees:\nTime of flight is %10.1e s\nThe range in x-direction is %10.1e m\nMaximum height is %10.1e m\nThe speed at maximum height is %10.1e m/s\nKinetic energy at the maximum height is %8.2e J" % (m,v0,theta,tf,R, hm,vh,Kh))
+    print("For a rock with %5.3f kg mass thrown with %5.3f m/s at an angle of %6.2f degrees:\nTime of flight is %10.1e s\nThe range in x-direction is %10.1e m\nMaximum height is %10.1e m\nThe speed at maximum height is %10.1e m/s\nKinetic energy at the maximum height is %8.2e J" % (m,v0,theta,tf,R, hm,vh,Kh))
     return tf, R, hm, vh,Kh
 my_result= throw_rock(1.5,0.3,35.20)
